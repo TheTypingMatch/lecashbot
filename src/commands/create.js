af49b02bccd9932b0d67a3e02e9e1e8893cdb908
Ryan Harrington <rharrington.web@gmail.com>
2020-03-21 20:32:26 -0600

    create profile command date object addition

--- src/commands/create.js
+++ src/commands/create.js
@@ -1,3 +1,24 @@
+const User = require('../models/user.model')
+
+const validateLink = link => {
+    return true
+}
+
 module.exports = (msg, client, args) => {
-    return 
+    const ntLink = args[0]
+    if (validateLink(ntLink)) {
+        const user = new User({
+            date: new Date(),
+            name: msg.author.username, 
+            nitroTypeLink: ntLink, 
+            discordId: msg.author.id,
+            discordTag: msg.author.tag
+        })
+        user.save()
+            .then(() => {
+                msg.reply('Success!')
+            }).catch(err => {
+                msg.reply('Error!')
+            })
+    }
 }
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose')
 
 const articleSchema = new mongoose.Schema({
+    date: {
+        type: Object, 
+        required: true
+    },
     name: {
         type: String, 
         required: true
