48fedf9cb82a7c31215e0d84eb33e484da1feac1
Ryan Harrington <rharrington.web@gmail.com>
2020-04-05 16:10:02 -0600

    v1.0.1

--- config/config.js
+++ config/config.js
@@ -15,7 +15,7 @@ module.exports = {
         yellow: 0xF09F19, 
         green: 0x40AC7B
     },
-    version: '1.0.0',
+    version: '1.0.1',
     token: process.env.TOKEN,
     prefix: '$',
     devMode: false, 
