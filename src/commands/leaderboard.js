1236bcbf1ea5b9fdcc2e19f0cc7478955c189c98
Ryan Harrington <rharrington.web@gmail.com>
2020-04-19 13:14:18 -0600

    refactor: leaderboard.js

--- src/commands/leaderboard.js
+++ src/commands/leaderboard.js
@@ -37,10 +37,7 @@ const handleCashLb = (msg, users) => {
     const sortedUsers = users.sort((a, b) => (a.balance > b.balance) ? 1 : ((b.balance > a.balance) ? -1 : 0))
     const topTen = getTopTen(sortedUsers)
     
-    topTen.forEach((user, pos) => {
-        desc += `#**${pos + 1}** ${user.name} - $**${currency(user.balance)}**\n`
-    })
-
+    topTen.forEach((user, pos) => desc += `#**${pos + 1}** ${user.name} - $**${currency(user.balance)}**\n`)
     lbEmbed.setDescription(desc)
     desc = ''
 
@@ -51,9 +48,7 @@ const handleCashLb = (msg, users) => {
 module.exports = async (msg, client, args) => {
 
     const users = await User.find({ banned: false })
-    
-    if (!users)
-    log('error', `ERROR: DB could not find users:\n**${users}**`)
+    if (!users) log('error', `ERROR: DB could not find users:\n**${users}**`, client)
     
     return (args[0] == 'bet') ? handleBetLb(msg, users) : handleCashLb(msg, users)
 
