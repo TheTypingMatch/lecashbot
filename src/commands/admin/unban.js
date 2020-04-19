7874b71f0f1cdc710dbbaec3aa54a331871e1b11
Ryan Harrington <rharrington.web@gmail.com>
2020-04-19 12:48:12 -0600

    refactor: unban.js

--- src/commands/admin/unban.js
+++ src/commands/admin/unban.js
@@ -1,27 +1,27 @@
 const User = require('../../models/user.model')
-const log = require('../../utils/log')
+const checkErr = require('../../utils/checkErr')
 const { RichEmbed } = require('discord.js')
 const { colors, version } = require('../../../config/config')
 
-module.exports = async (msg, client, args) => {
+module.exports = async ({ author, channel }, client, args) => {
 
     const err = 'This user does not have an account!'
-    const userId = args[0] ? args[0].replace(/<|@|!|>/g, '') : msg.author.id
+    const userId = args[0] ? args[0].replace(/<|@|!|>/g, '') : author.id
     const id = { discordId: userId }
     const user = await User.findOne(id)
     let result = (user && !user.dev) ? `${user.name} has been unbanned.` : err
     
     if (!user.dev)
-        User.update(id, { banned: false }, e => log('error', e, client))
+        User.update(id, { banned: false }, e => checkErr(e, client))
     else result = 'You can\'t unban a developer/admin!'
 
     let unbanEmbed = new RichEmbed()
         .setColor(colors.green)
-        .setAuthor('Unban', msg.author.avatarURL)
+        .setAuthor('Unban', author.avatarURL)
         .setTimestamp(new Date())
         .setFooter(`LeCashBot v${version}`)
         .setDescription(result)
 
-    return msg.channel.send(unbanEmbed)
+    return channel.send(unbanEmbed)
 
 }
