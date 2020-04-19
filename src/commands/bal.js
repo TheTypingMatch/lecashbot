5e1add4954efef2679ac0d0f2b1e9ce4f49ae905
Ryan Harrington <rharrington.web@gmail.com>
2020-04-19 12:50:33 -0600

    refactor: bal.js

--- src/commands/bal.js
+++ src/commands/bal.js
@@ -3,10 +3,10 @@ const { RichEmbed } = require('discord.js')
 const { colors, version } = require('../../config/config')
 const { currency } = require('../utils/format')
 
-module.exports = async (msg, client, args) => {
+module.exports = async ({ author, channel }, client, args) => {
 
     const err = 'This user does not have an account!'
-    const userId = args[0] ? args[0].replace(/<|@|!|>/g, '') : msg.author.id
+    const userId = args[0] ? args[0].replace(/<|@|!|>/g, '') : author.id
     
     const isNotId = (isNaN(parseInt(userId)))
     const userData = (isNotId) ? { name: args.join(' ').trim() } : { discordId: userId }
@@ -17,11 +17,11 @@ module.exports = async (msg, client, args) => {
 
     let balanceEmbed = new RichEmbed()
         .setColor(colors.green)
-        .setAuthor('Balance', userAvatar ? userAvatar.avatarURL : msg.author.avatarURL)
+        .setAuthor('Balance', userAvatar ? userAvatar.avatarURL : author.avatarURL)
         .setTimestamp(new Date())
         .setFooter(`LeCashBot v${version}`)
         .setDescription(result)
 
-    return msg.channel.send(balanceEmbed)
+    return channel.send(balanceEmbed)
 
 }
