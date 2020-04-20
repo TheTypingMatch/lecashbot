3170cd24f526572613ab3bd2daf7880169e90f03
Ryan Harrington <rharrington.web@gmail.com>
2020-04-20 14:21:00 -0600

    v1.2.1

--- config/config.js
+++ config/config.js
@@ -22,7 +22,7 @@ module.exports = {
         ],
         error: ['692123000602099712']
     },
-    version: '1.2.0',
+    version: '1.2.1',
     token: process.env.TOKEN,
     prefix: '$',
     devMode: false, 
