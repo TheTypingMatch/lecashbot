a040e3f01d05193c5ea52d2704c880d79c5f674c
Ryan Harrington <rharrington.web@gmail.com>
2020-05-07 17:02:56 -0600

    link verification for registration command

--- src/commands/register.js
+++ src/commands/register.js
@@ -1,14 +1,41 @@
 const User = require('../models/user.model')
 const log = require('../utils/log')
+const fetch = require('node-fetch')
+const checkErr = require('../utils/checkErr')
 
-const validateLink = link => !!link.includes('nitrotype.com/racer/')
+const urlExists = async url => {
+    return await fetch(url)
+        .then(res => res.text())
+        .then(res => res.includes('RACER_INFO'))
+        .catch(err => checkErr(err))
+}
+
+const register = (msg, ntLink) => {
+    const user = new User({
+        date: new Date(),
+        name: msg.author.username,
+        nitroTypeLink: ntLink,
+        discordId: msg.author.id,
+        discordTag: msg.author.tag
+    })
+    user.save(err => {
+        if (err) {
+            log('error', err, client)
+            return msg.reply('Error creating account. Contact a LeCashBot dev!')
+        } else return msg.reply('Success! See `$help` for information on commands')
+    })
+}
 
 module.exports = async (msg, client, args) => {
-    if (!args[0]) return msg.reply('Undefined NitroType profile: Use `$register https://www.nitrotype.com/racer/YOUR_USERNAME`')
+    if (!args[0]) {
+        return msg.reply('Undefined NitroType profile: Use `$register https://www.nitrotype.com/racer/YOUR_USERNAME`')
+    }
 
     // Check if someone is already registered with this NitroType link
     const linkExists = await User.findOne({ nitroTypeLink: args[0] })
-    if (linkExists) return msg.reply('Someone is already registered with this account!')
+    if (linkExists) {
+        return msg.reply('Someone is already registered with this account!')
+    }
 
     // Check if the user already has an account
     const userExists = await User.findOne({
@@ -17,21 +44,12 @@ module.exports = async (msg, client, args) => {
 
     if (userExists) return msg.reply('You already have an account!')
 
-    // validate link
-    const ntLink = args[0]
-    if (validateLink(ntLink)) {
-        const user = new User({
-            date: new Date(),
-            name: msg.author.username,
-            nitroTypeLink: ntLink,
-            discordId: msg.author.id,
-            discordTag: msg.author.tag
-        })
-        user.save(err => {
-            if (err) {
-                log('error', err, client)
-                return msg.reply('Error creating account. Contact a LeCashBot dev!')
-            } else return msg.reply('Success! See `$help` for information on commands')
-        })
-    } else msg.reply('Invalid NitroType profile link!')
+    const link = (args[0].includes('nitrotype.com/racer/'))
+        ? args[0]
+        : `https://www.nitrotype.com/racer/${args[0]}`
+
+    const ntLinkExists = await urlExists(link)
+    return (ntLinkExists)
+        ? register(msg, link)
+        : msg.reply('Invalid NitroType profile link!')
 }
