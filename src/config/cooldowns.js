9527ac95bfa61b9f296c3301f4b91db9dab8955d
Ryan Harrington <rharrington.web@gmail.com>
2020-05-09 13:07:03 -0600

    2 minute msg cooldown

--- src/config/cooldowns.js
+++ src/config/cooldowns.js
@@ -1,7 +1,7 @@
 // All cooldowns are in seconds
 
 module.exports = {
-    msgCooldown: 60000,
+    msgCooldown: 120000,
     daily: 1000 * 3600 * 21,
     dailyReset: 1000 * 3600 * 36,
     betTime: 1000 * 3
