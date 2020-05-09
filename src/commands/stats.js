b2edef636cf1b2eabfd40d3908f9d17b2bdd87c8
Ryan Harrington <rharrington.web@gmail.com>
2020-05-08 20:35:25 -0600

    stats command header update

--- src/commands/stats.js
+++ src/commands/stats.js
@@ -4,7 +4,7 @@ const { colors, version } = require('../config/config')
 module.exports = async (msg, client, args) => {
     const reportEmbed = new RichEmbed()
         .setColor(colors.green)
-        .setAuthor('Report', msg.author.avatarURL)
+        .setAuthor('Stats', msg.author.avatarURL)
         .setTimestamp(new Date())
         .setFooter(`LeCashBot v${version}`)
         .setDescription(`
