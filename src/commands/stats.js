09882d05b8a9e445ac816e1302a1628fc3754866
Ryan Harrington <rharrington.web@gmail.com>
2020-05-18 09:31:12 -0600

    stats command: large values have commas between every 3 digits

--- src/commands/stats.js
+++ src/commands/stats.js
@@ -1,16 +1,18 @@
 const { MessageEmbed } = require('discord.js')
 const { colors, version } = require('../config/config')
+const { currency } = require('../utils/format')
 
 module.exports = async (msg, client, args) => {
+    const { guilds, users, ws } = client
     const statsEmbed = new MessageEmbed()
         .setColor(colors.green)
         .setAuthor('Stats', msg.author.avatarURL())
         .setTimestamp(new Date())
         .setFooter(`LeCashBot v${version}`)
         .setDescription(`
-            Servers - **${client.guilds.cache.size}**
-            Users   - **${client.users.cache.size}**
-            Latency - **${Math.round(client.ws.ping)}**ms
+            Servers - **${currency(guilds.cache.size)}**
+            Users   - **${currency(users.cache.size)}**
+            Latency - **${Math.round(ws.ping)}**ms
         `)
 
     return msg.channel.send(statsEmbed)
