73ac2a5859a3b28c67f35b438f8981d267d1d76b
Ryan Harrington <rharrington.web@gmail.com>
2020-05-27 22:57:16 -0600

    v1.3.3

--- src/config/config.js
+++ src/config/config.js
@@ -15,7 +15,7 @@ module.exports = {
         ],
         error: ['692123000602099712']
     },
-    version: '1.3.2',
+    version: '1.3.3',
     prefix: '$',
     devMode: false,
     logEnabled: true
