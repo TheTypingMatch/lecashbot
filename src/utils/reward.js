b4cf52b273058a609915268b5b1697cb629ee086
Ryan Harrington <rharrington.web@gmail.com>
2020-05-30 12:07:02 -0600

    fix: modified random reward for large donors

--- src/utils/reward.js
+++ src/utils/reward.js
@@ -2,10 +2,11 @@ const User = require('../models/user.model')
 const log = require('../utils/log')
 
 const reward = async (userId, client) => {
-    const randReward = Math.floor(Math.random() * 50 + 25)
+    let randReward = Math.floor(Math.random() * 50 + 25)
     const user = await User.findOne({ discordId: userId })
 
     if (!user) return log('error', `User not found: ${user}`)
+    if (user.donations > 10 ** 8) randReward *= 2
 
     const userBalance = user.balance
     User.updateOne({ discordId: userId }, {
