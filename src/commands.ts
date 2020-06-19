ea3561a345bb92913b98150432f552ac2ffc3fd9
Ryan Harrington <rharrington.web@gmail.com>
2020-06-18 20:29:09 -0600

    refactor: command handler

--- src/commands.ts
+++ src/commands.ts
@@ -6,12 +6,12 @@ const run = async (cmd: string, msg, client, args: string[]) => {
     const user: any = await User.findOne({ discordId: msg.author.id })
     const generalPath: string = path.resolve(`./build/commands/${cmd}.js`)
     const adminPath: string = path.resolve(`./build/commands/admin/${cmd}.js`)
-    const hasAdminPerms: boolean = (fs.existsSync(adminPath) && user.admin)
+    const hasAdminPerms: boolean = (fs.existsSync(adminPath) && user?.admin)
 
     if (hasAdminPerms) {
         const adminCMD = await import(`./commands/admin/${cmd}`)
         return adminCMD.default(msg, client, args)
-    } else if (fs.existsSync(generalPath)){
+    } else if (fs.existsSync(generalPath)) {
         const generalCMD = await import(`./commands/${cmd}`)
         return generalCMD.default(msg, client, args)
     }
