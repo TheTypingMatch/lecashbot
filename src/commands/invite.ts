524387551f453669f9dc8ca69acba59692f40d5b
Ryan Harrington <rharrington.web@gmail.com>
2020-10-15 19:01:09 -0600

    fix: updated invite link

--- src/commands/invite.ts
+++ src/commands/invite.ts
@@ -7,7 +7,7 @@ export default async (msg, client, args) => {
         .setAuthor('Invite', msg.author.avatarURL())
         .setTimestamp(new Date())
         .setFooter(`LeCashBot v${version}`)
-        .setDescription('Invite LeCashBot [**here**](https://le-sirh.github.io/lecashbot/).')
+        .setDescription('Invite LeCashBot [**here**](https://lesirh.github.io/lecashbot/).')
 
     return msg.channel.send(inviteEmbed)
 }
