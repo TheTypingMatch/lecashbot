7d2ef283273c77b27a236d2e487d0772bb0531fd
Ryan Harrington <rharrington.web@gmail.com>
2020-12-17 22:50:28 -0700

    perf: removed cash logging in channels

--- src/utils/log.ts
+++ src/utils/log.ts
@@ -3,35 +3,6 @@ import { colors, logEnabled, channels, version } from '../config/config';
 
 const Discord = require('discord.js');
 
-const sendErrorEmbed = (msg: string, client) => {
-    const errEmbed = new Discord.MessageEmbed()
-        .setColor(colors.red)
-        .setAuthor('ERROR')
-        .setTimestamp(new Date())
-        .setFooter(`LeCashBot v${version}`)
-        .setDescription(`\`\`\`js\n${msg}\`\`\``);
-
-    client.logger.log(msg, 'error');
-
-    const errChannels: string[] = channels.error;
-    return errChannels.forEach(channel => {
-        const errChannel: any = client.channels.cache.get(channel);
-        if (errChannel) errChannel.send(errEmbed);
-    });
-};
-
-const logEarnings = (msg: string, client) => {
-    const logEmbed = new Discord.MessageEmbed()
-        .setColor(colors.green)
-        .setDescription(msg);
-
-    client.logger.log(msg.replace(/\*/g, ''));
-    return channels.log.forEach(channel => {
-        const logChannel: any = client.channels.cache.get(channel);
-        if (logChannel) logChannel.send(logEmbed);
-    });
-};
-
 const log = (type: string, msg: string, client) => {
     if (logEnabled) {
         fs.appendFile(`./logs/${type}.log`, `${msg} - ${new Date()}\n`, err => {
@@ -39,9 +10,9 @@ const log = (type: string, msg: string, client) => {
                 console.log(err);
             }
         });
-        switch (type) {
-            case 'error': return sendErrorEmbed(msg, client);
-            case 'cash': return logEarnings(msg, client);
+
+        if (type === 'cash') {
+            client.logger.log(msg.replace(/\*/g, ''));
         }
     }
 };
