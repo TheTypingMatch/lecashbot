fdd6c9732ce4ef1c9031a5d50b53e35be2737bab
Ryan Harrington <rharrington.web@gmail.com>
2020-12-18 13:50:42 -0700

    feat: added ban logs

--- src/commands/admin/ban.ts
+++ src/commands/admin/ban.ts
@@ -9,6 +9,8 @@ export default async ({ author, channel }, client, args) => {
     const user: any = await User.findOne(id);
     let result: string = (user && !user.dev) ? `${user.name} has been banned.` : err;
 
+    client.logger.ready(`${user.name} has been banned. (Moderator: ${author.id})`);
+
     if (!user.dev && !user.admin) {
         User.update(id, { banned: true });
     } else result = 'You can\'t ban a developer/admin!';
