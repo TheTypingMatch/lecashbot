aa3c633850848047b28fb55495d78735e1ec5d51
Ryan Harrington <rharrington.web@gmail.com>
2020-12-20 16:38:13 -0700

    (WIP) feat: auto-update lotteries

--- src/commands/lottery.ts
+++ src/commands/lottery.ts
@@ -3,40 +3,84 @@ import { Lottery } from '../models/lottery.model';
 import { MessageEmbed } from 'discord.js';
 import { colors, version } from '../config/config';
 import { currency } from '../utils/format';
+import { msgCooldown } from '../config/cooldowns';
 
-const lotteryEmbed = new MessageEmbed()
-    .setColor(colors.green)
-    .setFooter(`LeCashBot v${version}`)
-    
+let entryFees = {
+    daily: 1000,
+    weekly: 5000,
+    monthly: 25000
+}
 
 // capitalize first letter
 const capitalize = (str: string) => str.charAt(0).toUpperCase() + str.slice(1);
 
 // calculate time between now and end date
-const timeLeft = endDate => {
-    return 0;
+const subtractDate = endDate => {
+    const now = new Date();
+    return endDate.getTime() - now.getTime();
+}
+
+const enterUser = async (client, msg, type, cost) => {
+    const lottery = await Lottery.findOne({ type });
+    const user = await User.findOne({ discordId: msg.author.id });
+
+    if (lottery.entries.includes(msg.author.id)) {
+        return msg.reply('You are already entered!');
+    }
+
+    if (user.balance < cost) {
+        return msg.reply('You do not have enough to enter this lottery.');
+    }
+    
+    client.logger.log(`(${msg.author.id}) entered the ${type} lottery.`)
+    User.updateOne({ discordId: msg.author.id }, {
+        balance: user.balance - cost
+    });
+
+    await Lottery.updateOne({ type }, {
+        entries: [...lottery.entries, msg.author.id]
+    });
+
+    return msg.reply('You have been entered! 🍀');
 }
 
 // format time to dd:hh:mm:ss
-const formatTime = date => {
-    return '';
+const formatTime = ms => {
+    let seconds = parseInt((ms / 1000) % 60);
+    let minutes = parseInt((ms / (1000 * 60)) % 60);
+    let hours = parseInt((ms / (1000 * 60 * 60)) % 24);
+    const days = parseInt((ms / (1000 * 60 * 60 * 24)) % 365);
+
+    hours = (hours < 10) ? "0" + hours : hours;
+    minutes = (minutes < 10) ? "0" + minutes : minutes;
+    seconds = (seconds < 10) ? "0" + seconds : seconds;
+
+    return `${days ? `${days}:` : ''}${hours}:${minutes}:${seconds}`;
 }
 
 export default async (msg, client, args) => {
-    lotteryEmbed
-        .setAuthor('Lottery!', userAvatar ? userAvatar.avatarURL() : author.avatarURL())
+    const lotteryTypes = ['daily', 'weekly', 'monthly'];
+    if (args[1] === 'enter' && lotteryTypes.includes(args[0])) {
+        return await enterUser(client, msg, args[0], entryFees[args[0]]);
+    }
+    const lotteryEmbed = new MessageEmbed()
+        .setColor(colors.green)
+        .setFooter(`LeCashBot v${version}`)
+        .setAuthor('Lottery!', msg.author.avatarURL())
         .setTimestamp(new Date());
         
     let description = '';
-    const lotteryTypes = ['daily', 'weekly', 'monthly'];
-    for (type of lotteryTypes) {
+    for (let type of lotteryTypes) {
         const lottery = await Lottery.findOne({ type });
-        const timeLeft: string = formatTime(timeLeft(lottery.endDate));
+        const timeLeft: string = formatTime(subtractDate(lottery.endDate));
+        const prizePool: number = (lottery.entryFee || 0) * (lottery.entries.length || 0);
         
-        lotteryEmbed.addField(`**${capitalize(type)}**`, `
+        lotteryEmbed.addField(`
+            **${capitalize(type)}**`, `
             Ends in: \`${timeLeft}\`
-            Entries: **${lottery.entries}**
-            Prize Pool: $**${currency(lottery.entryFree * lottery.entries)}**
+            Entries: **${lottery.entries.length}**
+            Entry Fee: **$${currency(lottery.entryFee)}**
+            Prize Pool: $**${currency(prizePool) || 'Error'}**
         `);
     }
 
@@ -45,7 +89,7 @@ export default async (msg, client, args) => {
         \`$lottery <type> enter\` (e.g. \`$lottery daily enter\`)
     `);
 
-    return channel.send(lotteryEmbed);
+    return msg.channel.send(lotteryEmbed);
 };
 
 /*
