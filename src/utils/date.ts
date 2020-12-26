ae0691d2fbdbf96ae4f250bb43f6ed11c40240b7
Ryan Harrington <rharrington.web@gmail.com>
2020-12-25 18:39:13 -0700

    fix: formatted time rounding error

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -4,10 +4,10 @@ const toDays = (date: number) => ((date / 1000) / 3600) / 24;
 
 // format time to dd:hh:mm:ss
 const formatTime = ms => {
-    let seconds: string | number = (ms / 1000) % 60;
-    let minutes: string | number = (ms / (1000 * 60)) % 60;
-    let hours: string | number = (ms / (1000 * 60 * 60)) % 24;
-    const days: string | number = (ms / (1000 * 60 * 60 * 24)) % 365;
+    let seconds: string | number = Math.round((ms / 1000) % 60);
+    let minutes: string | number = Math.round((ms / (1000 * 60)) % 60);
+    let hours: string | number = Math.round((ms / (1000 * 60 * 60)) % 24);
+    const days: string | number = Math.round((ms / (1000 * 60 * 60 * 24)) % 365);
 
     hours = (hours < 10) ? '0' + hours : hours;
     minutes = (minutes < 10) ? '0' + minutes : minutes;
