0565603dad252d837c34ebf49480c1786ad229ec
Ryan Harrington <46948579+ryan-harrington@users.noreply.github.com>
2021-05-20 18:37:08 -0600

    fix: Capitalize lottery entered key value

--- src/commands/lottery.js
+++ src/commands/lottery.js
@@ -61,7 +61,7 @@ export default async (msg, client, args) => {
             **${capitalize(type)}**`, `
             Ends in: ${timeLeft}
             Entries: **${lottery.entries.length}**
-            Entered: **${lottery.entries.includes(msg.author.id) ? 'yes' : 'no'}**
+            Entered: **${lottery.entries.includes(msg.author.id) ? 'Yes' : 'No'}**
             Entry Fee: **$${currency(lottery.entryFee)}**
             Prize Pool: $**${currency(prizePool) || 'Error'}**
             Previous Winner: ${previousWinner}
