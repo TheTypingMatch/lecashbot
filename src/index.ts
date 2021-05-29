17feb0d780df9daa93ef280b12e4c978d51caa54
Damien Vesper <ldamienvesper@gmail.com>
2021-05-29 12:43:43 -0500

    add some nice logging

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,8 @@ import { Client } from './types/discord';
 
 import * as mongoose from 'mongoose';
 
-import config from '../config/config';
-
-import log from './utils/log';
 import * as loader from './utils/loader';
-
-mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
+import log from './utils/log';
 
 const client: Client = new Discord.Client({
     disableMentions: `everyone`,
@@ -21,11 +14,23 @@ const client: Client = new Discord.Client({
 // Uncaught exception handler.
 process.on(`uncaughtException`, e => log(`red`, e.stack));
 
+// Bot startup.
 const startBot = async () => {
+    await mongoose.connect(process.env.MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
+
+    log(`green`, `Connected to database.`);
+    
     await loader.loadCommands(client);
+    log(`green`, `All commands loaded!`);
+
     await loader.loadEvents(client);
+    log(`green`, `All events loaded!`);
 
     await client.login(process.env.DISCORD_TOKEN).catch(() => log(`red`, `Failed to authenticate client with application.`));
 };
 
+// Actually start the bot.
 startBot();
