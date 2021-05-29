9822feb1d23395887a97a4bda930cb4070323b7f
Damien Vesper <ldamienvesper@gmail.com>
2021-05-29 18:41:50 -0500

    run the util scripts on boot

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,17 +1,23 @@
 import { Client } from '../types/discord';
-import refreshActivity from '../utils/refreshActivity';
 
 import log from '../utils/log';
 import { logHeader } from '../utils/logExtra';
+
+import refreshActivity from '../utils/refreshActivity';
+import resetDailyStreak from '../utils/resetDailyStreak';
+import updateLeaderboards from '../utils/updateLeaderboards';
 
 export default async (client: Client) => {
     log(`green`, `Client has started, with ${client.users.cache.size} user(s) in ${client.guilds.cache.size} guild(s).`);
 
     logHeader(async () => {
-        // Update guild count every 10 minutes.
-        setInterval(() => refreshActivity(client), 6e5);
+        // Run utility scripts for the first time.
+        await refreshActivity(client);
 
-        // Then, set client activity for the first time.
-        refreshActivity(client);
+        await resetDailyStreak();
+        await updateLeaderboards(client);
+
+        setInterval(() => refreshActivity(client), 6e5); // Update guild count every 10 minutes.
+        setInterval(() => resetDailyStreak, 864e5); // Reset daily streak every 24 hours.
     });
 };
