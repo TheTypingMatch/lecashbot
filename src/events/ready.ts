d6d864d3735441de97b1b8ae7bc1cecf7aebcc7e
Damien Vesper <ldamienvesper@gmail.com>
2021-05-29 13:19:35 -0500

    optimism

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -2,7 +2,7 @@ import { Client } from '../types/discord';
 import log from '../utils/log';
 
 const callback = (client: Client) => {
-    log(`green`, `Client has started, with ${client.users.cache.size} users in ${client.guilds.cache.size} guilds.`);
+    log(`green`, `Client has started, with ${client.users.cache.size} user(s) in ${client.guilds.cache.size} guild(s).`);
 };
 
 export {
