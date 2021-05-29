ef70967f6529b5a4c5aff79ca8b15986a90ce772
Damien Vesper <ldamienvesper@gmail.com>
2021-05-29 15:12:45 -0500

    wrap it in a nice header

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,14 +1,17 @@
 import { Client } from '../types/discord';
+import refreshActivity from '../utils/refreshActivity';
 
 import log from '../utils/log';
-import refreshActivity from '../utils/refreshActivity';
+import { logHeader } from '../utils/logExtra';
 
 export default async (client: Client) => {
     log(`green`, `Client has started, with ${client.users.cache.size} user(s) in ${client.guilds.cache.size} guild(s).`);
 
-    // Update guild count every 10 minutes.
-    setInterval(() => refreshActivity(client), 36e6);
+    logHeader(async () => {
+        // Update guild count every 10 minutes.
+        setInterval(() => refreshActivity(client), 36e6);
 
-    // Then, set client activity for the first time.
-    refreshActivity(client);
+        // Then, set client activity for the first time.
+        refreshActivity(client);
+    });
 };
