8013afc69b6b223739ce204e341919822c4020e5
Damien Vesper <ldamienvesper@gmail.com>
2021-05-29 12:36:43 -0500

    clean up index into loader

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,17 @@
 import * as Discord from 'discord.js';
 import { Client } from './types/discord';
 
-import * as fs from 'fs';
-import * as path from 'path';
-
 import * as mongoose from 'mongoose';
 
 import config from '../config/config';
+
 import log from './utils/log';
+import * as loader from './utils/loader';
 
-import * as dotenv from 'dotenv';
-dotenv.config();
-
-mongoose.connect(config.db.uri, config.db.uriParams);
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 const client: Client = new Discord.Client({
     disableMentions: `everyone`,
@@ -22,32 +21,11 @@ const client: Client = new Discord.Client({
 // Uncaught exception handler.
 process.on(`uncaughtException`, e => log(`red`, e.stack));
 
-// Load events.
-fs.readdir(path.resolve(__dirname, `events`), async (err, files) => {
-    for (const file of files) {
-        if (err) log(`red`, err);
-        log(`yellow`, `Loaded event ${file}.`);
+const startBot = async () => {
+    await loader.loadCommands(client);
+    await loader.loadEvents(client);
 
-        const event = await import(`./events/${file}`);
-        client.on(file.split(`.`)[0], event.callback.bind(null, client));
-    }
-});
+    await client.login(process.env.DISCORD_TOKEN).catch(() => log(`red`, `Failed to authenticate client with application.`));
+};
 
-// Load commands.
-fs.readdir(path.resolve(__dirname, `events`), async (err, files) => {
-    for (const file of files) {
-        if (err) log(`red`, err);
-        log(`yellow`, `Loaded command ${file}.`);
-
-        const command = await import(`./commands/${file}`);
-        client.commands.push({
-            name: file.split(`.`)[0],
-            desc: command.desc,
-            usage: command.usage,
-            aliases: command.aliases,
-            run: command.run
-        });
-    }
-});
-
-client.login(process.env.DISCORD_TOKEN).catch(() => log(`red`, `Failed to authenticate client with application.`));
+startBot();
---
+++ src/utils/loader.ts
@@ -0,0 +1,40 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
+import { Client, Command, Event } from '../types/discord';
+import log from './log';
+
+const loadCommands = (client: Client) => {
+    fs.readdir(path.resolve(__dirname, `events`), async (err, files) => {
+        for (const file of files) {
+            if (err) log(`red`, err);
+            log(`yellow`, `Loaded event ${file}.`);
+
+            const event: Event = await import(`./events/${file}`);
+            client.on(file.split(`.`)[0], event.callback.bind(null, client));
+        }
+    });
+};
+
+const loadEvents = (client: Client) => {
+    fs.readdir(path.resolve(__dirname, `events`), async (err, files) => {
+        for (const file of files) {
+            if (err) log(`red`, err);
+            log(`yellow`, `Loaded command ${file}.`);
+
+            const command: Command = await import(`./commands/${file}`);
+            client.commands.push({
+                name: file.split(`.`)[0],
+                desc: command.desc,
+                usage: command.usage,
+                aliases: command.aliases,
+                run: command.run
+            });
+        }
+    });
+};
+
+export {
+    loadCommands,
+    loadEvents
+};
