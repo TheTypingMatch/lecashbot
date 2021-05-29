bcf84de11f4b970d850c6210f1db4f045079dffd
Damien Vesper <ldamienvesper@gmail.com>
2021-05-29 12:49:31 -0500

    add the extra log utils

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import * as Discord from 'discord.js';
-import { Client } from './types/discord';
-
 import * as mongoose from 'mongoose';
 
-import * as loader from './utils/loader';
+import { Client } from './types/discord';
 import log from './utils/log';
+
+import { logSplash } from './utils/logExtra';
+import * as loader from './utils/loader';
 
 const client: Client = new Discord.Client({
     disableMentions: `everyone`,
@@ -16,18 +17,21 @@ process.on(`uncaughtException`, e => log(`red`, e.stack));
 
 // Bot startup.
 const startBot = async () => {
-    await mongoose.connect(process.env.MONGODB_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    });
+    // Splash menu.
+    await logSplash();
 
     log(`green`, `Connected to database.`);
-    
+
     await loader.loadCommands(client);
     log(`green`, `All commands loaded!`);
 
     await loader.loadEvents(client);
     log(`green`, `All events loaded!`);
+
+    await mongoose.connect(process.env.MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
 
     await client.login(process.env.DISCORD_TOKEN).catch(() => log(`red`, `Failed to authenticate client with application.`));
 };
