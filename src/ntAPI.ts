b7f1f4c7df6975cf4c925da3234033359182c371
Damien Vesper <ldamienvesper@gmail.com>
2021-05-29 20:15:01 -0500

    set cookies on login

--- src/ntAPI.ts
+++ src/ntAPI.ts
@@ -9,6 +9,8 @@ class NTClient {
         password: string
     }
 
+    cookies: string[]
+
     constructor (username: string, password: string) {
         this.credentials = {
             username,
@@ -23,6 +25,9 @@ class NTClient {
         }).then(({ headers }) => {
             for (const str of headers[`set-cookie`]) {
                 const cookieObj = cookie.parse(str);
+                const key = Object.keys(cookieObj)[0];
+
+                this.cookies[key] = cookieObj[key];
             }
         });
     }
