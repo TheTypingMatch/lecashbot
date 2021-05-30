daf1134c66a209c43ce40127fe595a1be69076db
Damien Vesper <ldamienvesper@gmail.com>
2021-05-30 15:19:07 -0500

    start the rename command and add some things

--- config/colors.ts
+++ config/colors.ts
@@ -1,7 +1,26 @@
 const colors = {
-    danger: 0xf82055,
-    success: 0x47d66d,
-    info: 0xcfcf53
+    blue: 0x0d6efd,
+    indigo: 0x6610f2,
+    purple: 0x6f42c1,
+    pink: 0xd63384,
+    red: 0xdc3545,
+    orange: 0xfd7e14,
+    yellow: 0xffc107,
+    green: 0x198754,
+    teal: 0x20c997,
+    cyan: 0x0dcaf0,
+    white: 0xfff,
+    gray: 0x6c757d,
+    darkGray: 0x343a40,
+
+    primary: 0x0d6efd,
+    secondary: 0x6c757d,
+    success: 0x198754,
+    info: 0x0dcaf0,
+    warning: 0xffc107,
+    danger: 0xdc3545,
+    light: 0xf8f9fa,
+    dark: 0x212529
 };
 
 export default colors;
---
+++ src/commands/rename.ts
@@ -0,0 +1,52 @@
+import * as Discord from 'discord.js';
+import { Client, CommandConfig } from '../types/discord';
+
+import config from '../../config/config';
+
+import User from '../models/user.model';
+import randomString from '../utils/randomString';
+
+const cmd: CommandConfig = {
+    desc: `Change your linked NT account`,
+    usage: `<username>`
+};
+
+const run = async (client: Client, message: Discord.Message, args: string[]) => {
+    const m = `${message.author} »`;
+
+    const user = await User.findOne({ discordID: message.author.id });
+    if (!user) return message.channel.send(`${m} You do not have an account!`);
+
+    const verificationCode = randomString(4);
+
+    message.channel.send(`${m} Are you sure you want to change your linked NT account?\nType \`${verificationCode}\` to confirm doing so.`);
+
+    message.channel.awaitMessages(m => m.author.id === message.author.id, {
+        max: 1,
+        time: 1e4,
+        errors: [`time`]
+    }).then(msgs => {
+        if (msgs.first().content !== verificationCode) return message.channel.send(`${m} Invalid verification code. Aborting account deletion.`);
+
+        user.delete(err => {
+            if (err) {
+                log(`red`, err);
+                return message.channel.send(`${m} There was an error changing your linked NT account.\nPlease notify your local LeCashBot developer.`)
+            }
+        })
+
+        const sEmbed: Discord.MessageEmbed = new Discord.MessageEmbed()
+            .setColor(config.colors.warning)
+            .setAuthor(`Renamed Account`, message.author.avatarURL())
+            .setDescription(`Your NT account has been succesfully changed.\nWe are sorry to see you go.`)
+            .setTimestamp(new Date())
+            .setFooter(config.footer);
+
+        message.channel.send(sEmbed);
+    });
+};
+
+export {
+    cmd,
+    run
+};
