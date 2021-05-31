0a52d0ad01b26c5c666619326f41bc12f43ee6cc
Damien Vesper <ldamienvesper@gmail.com>
2021-05-30 19:16:54 -0500

    make formattime more readable
    you're welcome lesir

--- src/utils/formatTime.ts
+++ src/utils/formatTime.ts
@@ -1,12 +1,13 @@
-const formatTime = (num: number) =>
-    Math.abs(num) >= 864e5
-        ? `\`${(Math.abs(num) / 864e5).toFixed(2)}\` days`
-        : Math.abs(num) >= 36e5
-            ? `\`${(Math.abs(num) / 36e5).toFixed(2)}\` hours`
-            : Math.abs(num) >= 6e4
-                ? `\`${(Math.abs(num) / 6e4).toFixed(2)}\` minutes`
-                : Math.abs(num) >= 1e3
-                    ? `\`${(Math.abs(num) / 1e3).toFixed(2)}\` seconds`
-                    : `\`${Math.abs(num)}\` milliseconds`;
+const formatTime = (num: number) => {
+    let str = `\``;
+
+    if (Math.abs(num) >= 864e5) str += `${(Math.abs(num) / 864e5).toFixed(2)}\` days`;
+    else if (Math.abs(num) >= 36e5) str += `${(Math.abs(num) / 36e5).toFixed(2)}\` hours`;
+    else if (Math.abs(num) >= 6e4) str += `${(Math.abs(num) / 6e4).toFixed(2)}\` minutes`;
+    else if (Math.abs(num) >= 1e3) str += `${(Math.abs(num) / 1e3).toFixed(2)}\` seconds`;
+    else str += `${Math.abs(num)}\` milliseconds`;
+
+    return str;
+};
 
 export default formatTime;
