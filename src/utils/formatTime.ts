afacff893030a0f5f95b3bfa97e12733f2ec504f
Damien Vesper <ldamienvesper@gmail.com>
2021-05-30 19:06:09 -0500

    typescript handles types ahead of time

--- src/utils/formatTime.ts
+++ src/utils/formatTime.ts
@@ -1,11 +1,11 @@
 const formatTime = (num: number) =>
-    Math.abs(Number(num)) >= 864e5
-        ? `${(Math.abs(Number(num)) / 864e5).toFixed(2)} days`
-        : Math.abs(Number(num)) >= 36e5
-            ? `${(Math.abs(Number(num)) / 36e5).toFixed(2)} hours`
-            : Math.abs(Number(num)) >= 6e4
-                ? `${(Math.abs(Number(num)) / 6e4).toFixed(2)} minutes`
-                : Math.abs(Number(num)) >= 1e3
+    Math.abs(num) >= 864e5
+        ? `${(Math.abs(num) / 864e5).toFixed(2)} days`
+        : Math.abs(num) >= 36e5
+            ? `${(Math.abs(num) / 36e5).toFixed(2)} hours`
+            : Math.abs(num) >= 6e4
+                ? `${(Math.abs(num) / 6e4).toFixed(2)} minutes`
+                : Math.abs(num) >= 1e3
                     ? `${(Math.abs(num) / 1e3).toFixed(2)}\` seconds`
                     : `\`${Math.abs(num)}\` milliseconds`;
 
