7104e7ce2cb5098bc9300a47c72474d266d9ba07
Damien Vesper <ldamienvesper@gmail.com>
2021-05-31 10:03:27 -0500

    add the category to the command loader

--- src/utils/loader.ts
+++ src/utils/loader.ts
@@ -24,6 +24,7 @@ const loadCommands = (client: Client, callback?: any) => {
                 name: fileName,
                 config: {
                     desc: command.cmd.desc,
+                    category: command.cmd.category,
                     usage: command.cmd.usage || ``,
                     aliases: command.cmd.aliases || []
                 },
