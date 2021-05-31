ccaff27bb250b4a8e05400b355da850ba6bb8004
Damien Vesper <ldamienvesper@gmail.com>
2021-05-31 14:32:42 -0500

    update some donate

--- src/commands/donate.ts
+++ src/commands/donate.ts
@@ -4,6 +4,8 @@ import { Client, CommandConfig } from '../types/discord';
 import config from '../../config/config';
 import User from '../models/user.model';
 
+import { formatMoney } from '../utils/text';
+
 const cmd: CommandConfig = {
     desc: `Donate to the bot.`,
     category: `guides`,
@@ -16,6 +18,19 @@ const getDonors = async () => {
         discordID: donor.discordID,
         amount: donor.donations
     }));
+};
+
+const addField = async (userData: any[], client: Client) => {
+    userData.sort((a, b) => (a.amount > b.amount ? 1 : (b.amount > a.amount) ? -1 : 0));
+
+    let field = ``;
+    for (const userID of userData) {
+        const discUser = await client.users.fetch(userID);
+        field += `${discUser?.username || ``} ${userData.length > 5 ? ` ` : `\n`}
+        `;
+    }
+
+    return `${userData.map(user => `**${user.name}** - \`$${formatMoney(user.amount || 0)}\`\n`)}`.replace(/\n,/g, `\n`);
 };
 
 const run = async (client: Client, message: Discord.Message, args: string[]) => {};
