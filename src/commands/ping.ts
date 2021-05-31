7c1098658734f85d5fbb46c8d3e400a5013e373a
Damien Vesper <ldamienvesper@gmail.com>
2021-05-31 09:45:49 -0500

    add ping command

---
+++ src/commands/ping.ts
@@ -0,0 +1,26 @@
+import * as Discord from 'discord.js';
+import { Client, CommandConfig } from '../types/discord';
+
+import config from '../../config/config';
+
+const cmd: CommandConfig = {
+    desc: `View network latency between the bot and Discord.`
+};
+
+const run = async (client: Client, message: Discord.Message, args: string[]) => {
+    const msg = await message.channel.send(`Pong?`);
+
+    const sEmbed: Discord.MessageEmbed = new Discord.MessageEmbed()
+        .setColor(config.colors.orange)
+        .setAuthor(`Ping`, message.author.avatarURL())
+        .setDescription(`API: \`:${Math.abs(Math.round(client.ws.ping))}ms\`\nGateway: \`${Math.abs(Math.round(new Date().valueOf() - msg.createdTimestamp.valueOf()))}ms\``)
+        .setTimestamp(new Date())
+        .setFooter(config.footer);
+
+    msg.edit(sEmbed);
+};
+
+export {
+    cmd,
+    run
+};
