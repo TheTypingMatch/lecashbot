bf85b0f667c6fe5d8036c69a4b59d198df56d98c
Damien Vesper <ldamienvesper@gmail.com>
2021-06-09 16:21:00 -0500

    loader: use recursive directory reader

--- src/utils/loader.ts
+++ src/utils/loader.ts
@@ -1,4 +1,3 @@
-import * as fs from 'fs';
 import * as path from 'path';
 
 import { Client } from '../types/discord';
@@ -6,60 +5,58 @@ import { Client } from '../types/discord';
 import log from './log';
 import { logHeader } from './logExtra';
 
-const loadCommands = (client: Client, callback?: any) => {
+import readDirectory from './readDirectory';
+
+const loadCommands = async (client: Client, callback?: any) => {
     logHeader();
 
     // Initialize the commands array.
     client.commands = [];
 
-    fs.readdir(path.resolve(__dirname, `../commands`), async (err, files) => {
-        for (const file of files) {
-            if (err) log(`red`, err);
+    const files = readDirectory(path.resolve(__dirname, `../commands`));
 
-            const fileName = file.split(`.`)[0];
-            log(`yellow`, `Loaded command ${fileName}.`);
+    for (const file of files) {
+        const fileName = file.split(`\\`).pop().split(`.`)[0];
+        log(`yellow`, `Loaded command ${fileName}.`);
 
-            const command = await import(`../commands/${file}`);
-            client.commands.push({
-                name: fileName,
-                config: {
-                    desc: command.cmd.desc,
-                    category: command.cmd.category,
-                    usage: command.cmd.usage || ``,
-                    aliases: command.cmd.aliases || []
-                },
-                run: command.run
-            });
-        }
+        const command = await import(file);
+        client.commands.push({
+            name: fileName,
+            config: {
+                desc: command.cmd.desc,
+                category: command.cmd.category,
+                usage: command.cmd.usage || ``,
+                aliases: command.cmd.aliases || []
+            },
+            run: command.run
+        });
+    }
 
-        if (callback !== undefined) return callback();
-    });
+    if (callback !== undefined) return callback();
 };
 
-const loadEvents = (client: Client, callback?: any) => {
+const loadEvents = async (client: Client, callback?: any) => {
     logHeader();
 
     // Initialize client events.
     client.events = [];
 
-    fs.readdir(path.resolve(__dirname, `../events`), async (err, files) => {
-        for (const file of files) {
-            if (err) log(`red`, err);
+    const files = readDirectory(path.resolve(__dirname, `../events`));
 
-            const fileName = file.split(`.`)[0];
-            log(`yellow`, `Loaded event ${fileName}.`);
+    for (const file of files) {
+        const fileName = file.split(`\\`).pop().split(`.`)[0];
+        log(`yellow`, `Loaded event ${fileName}.`);
 
-            const event = await import(`../events/${file}`);
-            client.on(fileName, event.default.bind(null, client));
+        const event = await import(file);
+        client.on(fileName, event.default.bind(null, client));
 
-            client.events.push({
-                name: fileName,
-                callback: event
-            });
-        }
+        client.events.push({
+            name: fileName,
+            callback: event
+        });
+    }
 
-        if (callback !== undefined) return callback();
-    });
+    if (callback !== undefined) return callback();
 };
 
 export {
