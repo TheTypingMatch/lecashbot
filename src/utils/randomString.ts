80f12635ecb844ec38db7b8996ac0f7031f68977
Damien Vesper <ldamienvesper@gmail.com>
2021-06-12 13:12:21 -0500

    just toString it, no hex

--- src/utils/randomString.ts
+++ src/utils/randomString.ts
@@ -1,5 +1,5 @@
 import crypto from 'crypto';
 
-const randomString = (length: number) => crypto.randomBytes(length).toString(`hex`);
+const randomString = (length: number) => crypto.randomBytes(length).toString();
 
 export default randomString;
