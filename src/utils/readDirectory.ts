14c375f9f7507fd939f8a211ba5b1c888cf117d5
Damien Vesper <ldamienvesper@gmail.com>
2021-06-12 10:28:55 -0500

    clean up readdir

--- src/utils/readDirectory.ts
+++ src/utils/readDirectory.ts
@@ -8,7 +8,8 @@ const readDirectory = (dir: any) => {
     for (const file of files) {
         const filePath = path.resolve(dir, file);
         const stat = fs.statSync(filePath);
-        if (stat && stat.isDirectory()) {
+
+        if (stat?.isDirectory()) {
             const res = readDirectory(filePath);
             results = results.concat(res);
         } else results.push(filePath);
