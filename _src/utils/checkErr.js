f42b5cbbc2f85de1eca386d8a4f766a45c1d9217
Damien Vesper <ldamienvesper@gmail.com>
2021-06-12 12:59:57 -0500

    bye bye old src

--- _src/utils/checkErr.js
+++
@@ -1,8 +0,0 @@
-import { log } from './log';
-
-const checkErr = (err, client, success) => {
-    if (err) return log(`error`, err, client);
-    else if (success) return success();
-};
-
-export { checkErr };
