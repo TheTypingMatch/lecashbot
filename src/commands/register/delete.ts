ff05cc61b82b2c6ce69e9ca8209a932a10df6084
Damien Vesper <ldamienvesper@gmail.com>
2021-06-12 13:08:06 -0500

    prefer delete to deleteone

--- src/commands/register/delete.ts
+++ src/commands/register/delete.ts
@@ -35,7 +35,7 @@ const run = async (client: Client, message: Discord.Message, args: string[]) => 
             .setTimestamp(new Date())
             .setFooter(config.footer);
 
-        User.deleteOne({ discordID: message.author.id });
+        await user.delete();
         message.channel.send(sEmbed);
     }).catch(() => message.channel.send(`${m} You did not respond in time. Aborting account deletion.`));
 };
