1545230280d879402aee83e9eecebd6f9986b885
Damien Vesper <ldamienvesper@gmail.com>
2021-06-13 13:40:30 -0500

    await changes

--- src/commands/economy/lottery.ts
+++ src/commands/economy/lottery.ts
@@ -38,8 +38,8 @@ const enterUser = async (client: Client, message: Discord.Message, type: string,
     user.balance -= cost;
     lottery.entries.push(message.author.id);
 
-    user.save();
-    lottery.save();
+    await user.save();
+    await lottery.save();
 
     message.channel.send(`${m} You have entered the ${type} lottery!`);
 };
