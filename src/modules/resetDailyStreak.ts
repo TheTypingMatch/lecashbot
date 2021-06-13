29b9ee464ab4db74f9cadfd0eb7215a658ff40cf
Damien Vesper <ldamienvesper@gmail.com>
2021-06-13 18:44:13 -0500

    Revert "only reset daily streak if its above 0"
    This reverts commit 2b5cc8475a41c4691908cbad51ecb7c8c1f1cabd.

--- src/modules/resetDailyStreak.ts
+++ src/modules/resetDailyStreak.ts
@@ -7,10 +7,14 @@ const resetDailyStreak = async () => {
     log(`cyan`, `Updating daily streaks...`);
 
     const users = await User.find({ banned: false });
-    if (!users || users.length === 0) return log(`yellow`, `No active users found. Skipping`);
+    if (!users || users.length === 0) {
+        log(`yellow`, `No active users found. Skipping...`);
+
+        return;
+    }
 
     for (const user of users) {
-        if (user.streaks.daily > 0 && (new Date().valueOf() - new Date(user.cooldowns.daily).valueOf()) > config.cooldowns.utils.dailyReset) {
+        if ((new Date().valueOf() - new Date(user.cooldowns.daily).valueOf()) > config.cooldowns.utils.dailyReset) {
             log(`blue`, `Daily streak reset for user ${user.username} [${user.discordID}].`);
 
             user.streaks.daily = 0;
