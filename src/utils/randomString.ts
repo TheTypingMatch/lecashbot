6ff22b1bc166aac6e03f2b310e68d2b6fc5a6ab3
Damien Vesper <ldamienvesper@gmail.com>
2021-06-13 18:45:25 -0500

    Revert "just toString it, no hex"
    This reverts commit 80f12635ecb844ec38db7b8996ac0f7031f68977.

--- src/utils/randomString.ts
+++ src/utils/randomString.ts
@@ -1,5 +1,5 @@
 import crypto from 'crypto';
 
-const randomString = (length: number) => crypto.randomBytes(length).toString();
+const randomString = (length: number) => crypto.randomBytes(length).toString(`hex`);
 
 export default randomString;
