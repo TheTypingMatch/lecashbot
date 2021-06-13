2b5cc8475a41c4691908cbad51ecb7c8c1f1cabd
Damien Vesper <ldamienvesper@gmail.com>
2021-06-13 11:54:15 -0500

    only reset daily streak if its above 0

--- src/modules/resetDailyStreak.ts
+++ src/modules/resetDailyStreak.ts
@@ -7,14 +7,10 @@ const resetDailyStreak = async () => {
     log(`cyan`, `Updating daily streaks...`);
 
     const users = await User.find({ banned: false });
-    if (!users || users.length === 0) {
-        log(`yellow`, `No active users found. Skipping...`);
-
-        return;
-    }
+    if (!users || users.length === 0) return log(`yellow`, `No active users found. Skipping`);
 
     for (const user of users) {
-        if ((new Date().valueOf() - new Date(user.cooldowns.daily).valueOf()) > config.cooldowns.utils.dailyReset) {
+        if (user.streaks.daily > 0 && (new Date().valueOf() - new Date(user.cooldowns.daily).valueOf()) > config.cooldowns.utils.dailyReset) {
             log(`blue`, `Daily streak reset for user ${user.username} [${user.discordID}].`);
 
             user.streaks.daily = 0;
